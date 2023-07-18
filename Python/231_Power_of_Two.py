36f5d96fe0066ffe62ad25f2c2d37722fce3ab51
KaneZhao <hengchuanz123@163.com>
2023-07-18 13:27:20 +0930

    add solutions

---
+++ Python/231_Power_of_Two.py
@@ -0,0 +1,12 @@
+"""
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+"""
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+        return n % 2 == 0 and self.isPowerOfTwo(n // 2)         
