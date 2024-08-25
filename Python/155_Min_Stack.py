66afa898fe4e358375f59d49ebb71ef2ba9d7e19
KaneZhao <hengchuanz123@163.com>
2024-08-25 21:00:19 +0930

    add solutions

--- Python/155_Min_Stack.py
+++ Python/155_Min_Stack.py
@@ -10,35 +10,36 @@ int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
 """
+
 class MinStack:
 
     def __init__(self):
         self.s1 = []
-        self.min_val = None
+        self.s2 = []
 
     def push(self, val: int) -> None:
         if len(self.s1) == 0:
-            self.s1.append(val)
-            self.min_val = val
+            self.s2.append(val)
         else:
-            if val < min_val:
-                self.s1.append(self.min_val)
-                self.min_val = val
+            top = self.s2[-1]
+            if top < val:
+                self.s2.append(top)
             else:
-                self.s1.append(val)    
+                self.s2.append(val) 
+        self.s1.append(val)               
 
     def pop(self) -> None:
         if len(self.s1) == 0:
             raise IndexError()
         else:
-            top = self.s1.pop()
-            self.min_val = min(top, self.)   
+            self.s1.pop()
+            self.s2.pop()    
         
     def top(self) -> int:
         return self.s1[-1]
         
     def getMin(self) -> int:
-        return self.min_val
+        return self.s2[-1]
         
 
 
